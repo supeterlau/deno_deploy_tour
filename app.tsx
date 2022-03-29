e245eceff828928251d2e829d4a2e542fbc21b0f
Peter Lau <superpeterlau@outlook.com>
2022-03-30 01:25:02 +0800

    add chat

--- app.tsx
+++ app.tsx
@@ -1,6 +1,13 @@
 import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
 import { h, ssr, tw } from "https://crux.land/nanossr@0.0.1";
 
+const messages = [];
+
+const channel = new BroadcastChannel("chat");
+channel.onmessage = (event) => {
+  messages.push(event.data);
+};
+
 function handler(req: Request): Response {
   const url = new URL(req.url);
   console.log("path: ", url.pathname);
@@ -10,17 +17,27 @@ function handler(req: Request): Response {
       const name = url.searchParams.get("name") ?? "Deno";
       console.log("name: ", name);
       return ssr(() => <Front name={name} />);
-      break;
+    case "/send":
+      const message = url.searchParams.get("msg");
+      if (!message) {
+        return new Response("?msg not provided", { status: 400 });
+      }
+      messages.push(message);
+      channel.postMessage(message);
+      return new Response("message sent.");
+    case "/messages":
+      return new Response(JSON.stringify(messages), {
+        "content-type": "application/json",
+      });
     default:
       return new Response("Deno Deploy");
-      break;
   }
 }
 
 const Front = (props) => (
   <div class={tw`bg-white flex h-screen`}>
     <h3 class={tw`text-5xl text-gray-600 m-auto mt-20`}>
-      Welcome {props.name} !
+      Welcome to {props.name} !
     </h3>
   </div>
 );
