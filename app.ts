0b00e0a9ac32a1c29dd4b89624d1976ba1119b6a
Peter Lau <superpeterlau@outlook.com>
2022-03-29 23:55:11 +0800

    simple app.ts

---
+++ app.ts
@@ -0,0 +1,9 @@
+import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
+
+function handler(req: Request): Response {
+	return new Response('Deno Deploy');
+}
+
+console.log('Listening on http://localhost:8000')
+
+await serve(handler)
