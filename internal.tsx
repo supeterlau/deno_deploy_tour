31bc0e2ae9afe7df3ceb1512f80651e1d05a834d
Peter Lau <superpeterlau@outlook.com>
2022-06-24 14:36:44 +0800

    use deno std http server

--- internal.tsx
+++ internal.tsx
@@ -1,16 +1,20 @@
 // https://dev.to/anishkny/the-incredible-power-of-deno-deploy-p2c
 
-import serve from 'https://deno.land/std@0.145.0/http/server.ts';
+import {serve} from 'https://deno.land/std@0.145.0/http/server.ts';
 
 const handler = (req: Request): Response => {
   return new Response(
-	  `<pre style="font-size: 5rem">
-		  DENO_REGION=<br>
-			${Deno.env.get("DENO_REGION")}
-		</pre>`
-		{header: {"content-type": "text/html"}}
+	  `<pre style="font-size:3rem;">
+DENO_REGION=<br>
+${Deno.env.get("DENO_REGION")}
+</pre>`,
+		{headers: {"content-type": "text/html"}}
   );
 };
 
 console.log('Listen on http://999.999.999:0000');
-await serve(handler, {addr: "404 not found"})
+// await serve(handler, {hostname: "404 not found", port: 0})
+
+await serve(handler);
+
+// serve((_req) => new Response("Hello, world"));
